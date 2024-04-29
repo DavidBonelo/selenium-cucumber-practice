a50d00f478c1aebb7fe3d6f9cd034b04eb5765f5
David Bonelo <bonelodev@gmail.com>
2024-04-29 11:15:14 -0500

    add webdriver setup

---
+++ src/test/java/com/davidbonelo/setup/WebSetup.java
@@ -0,0 +1,45 @@
+package com.davidbonelo.setup;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+enum BrowserType {
+    CHROME, FIREFOX
+}
+
+public class WebSetup {
+    private static final String BASE_URL = "https://demoqa.com/";
+    protected WebDriver driver;
+
+    protected void setupDriver() {
+        driver = getBrowserDriver(BrowserType.FIREFOX);
+        driver.get(BASE_URL);
+        maximize();
+    }
+
+    private void maximize() {
+        driver.manage().window().maximize();
+    }
+
+    protected void quitDriver() {
+        driver.quit();
+    }
+
+    private WebDriver getBrowserDriver(BrowserType browserType) {
+        switch (browserType) {
+            case CHROME -> {
+                ChromeOptions co = new ChromeOptions();
+                co.addArguments("--remote-allow-origins=*");
+                co.addArguments("--incognito");
+                co.addArguments("--disable-notifications");
+                return new ChromeDriver(co);
+            }
+            case FIREFOX -> {
+                return new FirefoxDriver();
+            }
+            default -> throw new IllegalArgumentException("Incorrect browser type");
+        }
+    }
+}
--- src/test/java/com/davidbonelo/steps/ExampleSteps.java
+++ src/test/java/com/davidbonelo/steps/ExampleSteps.java
@@ -1,20 +1,24 @@
 package com.davidbonelo.steps;
 
+import com.davidbonelo.setup.WebSetup;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class ExampleSteps {
+public class ExampleSteps extends WebSetup {
     @Given("Example state")
     public void exampleState() {
+        setupDriver();
         System.out.println("Step1");
     }
 
     @When("Example action")
-    public void exampleAction() {
+    public void exampleAction() throws InterruptedException {
+        Thread.sleep(5000);
     }
 
     @Then("Example result")
     public void exampleResult() {
+        quitDriver();
     }
 }
